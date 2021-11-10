5c3979ec1a2d7b079675870991bb01913436f701
skovati <skovati@protonmail.com>
2021-11-10 15:11:11 -0600

    reorganize repo config

---
+++ lua/cybrpnk/colors.lua
@@ -0,0 +1,17 @@
+local lush = require('lush')
+local hsl = lush.hsl
+
+local colors = {
+    black = hsl("#171717"),
+    red = hsl("#ba423f"),
+    green = hsl("#94983a"),
+    yellow = hsl("#d7b44a"),
+    blue = hsl("#658fa0"),
+    magenta = hsl("#b080a4"),
+    cyan = hsl("#76ab97"),
+    white = hsl("#e8e4d9"),
+    grey = hsl("#6e6e6e"),
+    orange = hsl("#cb7437"),
+}
+
+return colors
--- lua/lualine/themes/cybrpnk.lua
+++ lua/lualine/themes/cybrpnk.lua
@@ -1,37 +1,29 @@
-require("cybrpnk/colors")
+local colors = require("cybrpnk.colors")
 
 local darkgrey = "#393939"
 local lightgrey = "#aeaeae"
 
 return {
-  normal = {
-    a = {bg = colors.grey, fg = colors.black, gui = 'bold'},
-    b = {bg = colors.lightgrey, fg = colors.white},
-    c = {bg = colors.darkgrey, fg = colors.grey}
-  },
-  insert = {
-    a = {bg = colors.blue, fg = colors.black, gui = 'bold'},
-    b = {bg = colors.lightgrey, fg = colors.white},
-    c = {bg = colors.lightgrey, fg = colors.white}
-  },
-  visual = {
-    a = {bg = colors.yellow, fg = colors.black, gui = 'bold'},
-    b = {bg = colors.lightgrey, fg = colors.white},
-    c = {bg = colors.lightgrey, fg = colors.black}
-  },
-  replace = {
-    a = {bg = colors.red, fg = colors.black, gui = 'bold'},
-    b = {bg = colors.lightgrey, fg = colors.white},
-    c = {bg = colors.black, fg = colors.white}
-  },
-  command = {
-    a = {bg = colors.green, fg = colors.black, gui = 'bold'},
-    b = {bg = colors.lightgrey, fg = colors.white},
-    c = {bg = colors.lightgrey, fg = colors.black}
-  },
-  inactive = {
-    a = {bg = colors.darkgrey, fg = colors.grey, gui = 'bold'},
-    b = {bg = colors.darkgrey, fg = colors.grey},
-    c = {bg = colors.darkgrey, fg = colors.grey}
-  }
+    normal = {
+        a = {bg = colors.grey, fg = colors.black, gui = 'bold'},
+        b = {bg = colors.lightgrey, fg = colors.white},
+        c = {bg = colors.darkgrey, fg = colors.grey}
+    },
+    insert = {
+        a = {bg = colors.blue, fg = colors.black, gui = 'bold'},
+    },
+    visual = {
+        a = {bg = colors.yellow, fg = colors.black, gui = 'bold'},
+    },
+    replace = {
+        a = {bg = colors.red, fg = colors.black, gui = 'bold'},
+    },
+    command = {
+        a = {bg = colors.green, fg = colors.black, gui = 'bold'},
+    },
+    inactive = {
+        a = {bg = colors.darkgrey, fg = colors.grey, gui = 'bold'},
+        b = {bg = colors.darkgrey, fg = colors.grey},
+        c = {bg = colors.darkgrey, fg = colors.grey}
+    }
 }
