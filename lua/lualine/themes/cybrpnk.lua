58bb5240562fae2e57f6b45b5f96003f7b27d44c
skovati <skovati@protonmail.com>
2021-11-10 15:50:06 -0600

    lualine changes

--- lua/lualine/themes/cybrpnk.lua
+++ lua/lualine/themes/cybrpnk.lua
@@ -5,24 +5,24 @@ local lightgray = "#aeaeae"
 
 return {
     normal = {
-        a = {fg = colors.white, bg = colors.blue, gui = 'bold'},
-        b = {fg = colors.gray, bg = colors.black},
-        c = {fg = colors.gray, bg = colors.black},
+        a = {fg = colors.white, bg = colors.blue, gui = "bold"},
+        b = {fg = lightgray, bg = darkgray},
+        c = {fg = lightgray, bg = colors.black},
     },
     insert = {
-        a = {fg = colors.white, bg = colors.red, gui = 'bold'},
+        a = {fg = colors.white, bg = colors.red, gui = "bold"},
     },
     visual = {
-        a = {fg = colors.white, bg = colors.cyan, gui = 'bold'},
+        a = {fg = colors.white, bg = colors.cyan, gui = "bold"},
     },
     replace = {
-        a = {fg = colors.white, bg = colors.magenta, gui = 'bold'},
+        a = {fg = colors.white, bg = colors.magenta, gui = "bold"},
     },
     command = {
-        a = {fg = colors.white, bg = colors.green, gui = 'bold'},
+        a = {fg = colors.white, bg = colors.green, gui = "bold"},
     },
     inactive = {
-        a = {fg = colors.gray, bg = darkgray, gui = 'bold'},
+        a = {fg = colors.gray, bg = darkgray, gui = "bold"},
         b = {fg = colors.gray, bg = darkgray},
         c = {fg = colors.gray, bg = darkgray},
     }
